acf840c2fc665fa9feae08e059c91207d9a0dacd
matrHS <matthew.r.hunt@gmail.com>
2026-02-13 12:09:49 +0100

    created entities for anomaly_set and anomaly_def

---
+++ lib/entity/anomaly_set.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+/// Represents each "set" of data included in a page of the main application.
+/// Each detected anomaly will be a set and the data on this set will be used
+/// to display the image, surrounding images and the anomaly definition.
+class AnomalySet {
+  final String imageName;
+  final Enum anomalyDef;
+  final double anomalyConf;
+  final int lineNumber;
+  final int imageNumber;
+
+  AnomalySet({
+    required this.imageName,
+    required this.anomalyDef,
+    required this.anomalyConf,
+    required this.lineNumber,
+    required this.imageNumber,
+  });
+}
