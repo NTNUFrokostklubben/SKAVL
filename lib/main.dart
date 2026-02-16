c5678f4c499d55b3ce979627c62c8697217ba916
matrHS <matthew.r.hunt@gmail.com>
2026-02-16 11:24:02 +0100

    fix duplicate title

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-
       title: 'SKAVL',
 
       theme: ThemeData(
