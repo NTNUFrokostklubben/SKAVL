e9af7cdcbf54d2cac90e245b47cdbac060d0e7db
mhuntno <me@mhunt.no>
2026-05-05 20:56:06 +0200

    added workflow for generating pdf report at given location with unique name. Added util for opening explorer on completion

--- lib/controller/report_generation_controller.dart
+++ lib/controller/report_generation_controller.dart
@@ -1,9 +1,12 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:grpc/grpc.dart';
 import 'package:skavl/entity/anomaly_set.dart';
 import 'package:skavl/entity/project_metadata.dart';
 import 'package:skavl/proto/report.pb.dart';
 import 'package:skavl/proto/report.pbgrpc.dart';
 import 'package:skavl/services/port_config_service.dart';
+import 'package:skavl/util/file_system_util.dart';
 
 import '../proto/anomaly.pb.dart' as pb2;
 
@@ -47,9 +50,13 @@ class ReportGenerationController {
           ),
           confidenceThreshold: projectMetadata.sensitivity,
           locale: locale,
+          saveLocation: await _pickPath(
+            projectMetadata,
+            classification: 'unclassified',
+          ),
         ),
       )
-      .then((r) => print(r));
+      .then((r) => FileSystemUtil.openDirectory(r.reportUrl));
 
   /// Generates an unclassified report based on projectName
   Future<void> generateClassifiedReport({
@@ -59,27 +66,51 @@ class ReportGenerationController {
     required String locale,
   }) async => await _client
       .generateReportClassified(
-    ReportGenerationRequest(
-      projectMetadata: pb2.ProjectMetadata(
-        projectName: projectMetadata.projectName,
-        sosiFilePath: projectMetadata.sosiFilePath,
-        imageFolderPath: projectMetadata.imageFolderPath,
-        sosiWaterMaskPath: projectMetadata.sosiWaterMaskPath,
-      ),
-      anomalySets: projectMetadata.anomaliesInRange.map(
+        ReportGenerationRequest(
+          projectMetadata: pb2.ProjectMetadata(
+            projectName: projectMetadata.projectName,
+            sosiFilePath: projectMetadata.sosiFilePath,
+            imageFolderPath: projectMetadata.imageFolderPath,
+            sosiWaterMaskPath: projectMetadata.sosiWaterMaskPath,
+          ),
+          anomalySets: projectMetadata.anomaliesInRange.map(
             (a) => pb2.AnomalySet(
-          imageNumber: a.imageNumber,
-          lineNumber: a.lineNumber,
-          imageName: a.imageName,
-          anomalyType: pb2.AnomalyTypes.valueOf(a.anomalyType.index),
-          userClassification: a.userClassification,
-          geotiffCoordinate: pb2.UtmCoordinate(easting: 0, northing: 0),
-          anomalyConfidence: a.anomalyConf,
+              imageNumber: a.imageNumber,
+              lineNumber: a.lineNumber,
+              imageName: a.imageName,
+              anomalyType: pb2.AnomalyTypes.valueOf(a.anomalyType.index),
+              userClassification: a.userClassification,
+              geotiffCoordinate: pb2.UtmCoordinate(easting: 0, northing: 0),
+              anomalyConfidence: a.anomalyConf,
+            ),
+          ),
+          confidenceThreshold: projectMetadata.sensitivity,
+          locale: locale,
+          saveLocation: await _pickPath(
+            projectMetadata,
+            classification: 'classified',
+          ),
         ),
-      ),
-      confidenceThreshold: projectMetadata.sensitivity,
-      locale: locale,
-    ),
-  )
-      .then((r) => print(r));
+      )
+      .then((r) => FileSystemUtil.openDirectory(r.reportUrl));
+
+  Future<String> _pickPath(
+    ProjectMetadata projectMetadata, {
+    required String classification,
+  }) async {
+    DateTime currentTime = DateTime.timestamp();
+    String? result = await FilePicker.saveFile(
+      dialogTitle: 'Save report',
+      fileName:
+          '${projectMetadata.projectName}-report-$classification-${currentTime.year}${currentTime.month}${currentTime.day}${currentTime.hour}${currentTime.minute}.pdf',
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (result != null) {
+      return result;
+    } else {
+      return "";
+    }
+  }
 }
